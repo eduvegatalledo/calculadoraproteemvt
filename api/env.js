6b058f1332669449b5e529f48cabef8d16e0387d
eduvegatalledo <evegat@pucp.edu.pe>
2025-09-09 12:10:40 -0500

    Delete api directory

--- api/env.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/javascript');
-  res.send(`window.env = {
-    SUPABASE_URL: "${process.env.SUPABASE_URL}",
-    SUPABASE_ANON_KEY: "${process.env.SUPABASE_ANON_KEY}"
-  };`);
-}
