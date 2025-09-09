375f0a75a8c2ac8bdbd123ce212a5c9913edcb4c
eduvegatalledo <evegat@pucp.edu.pe>
2025-09-09 12:21:04 -0500

    Delete src/macros.js

--- src/macros.js
+++
@@ -1,9 +0,0 @@
-export function calculateMacros(calories, proteinRatio, carbRatio, fatRatio) {
-  if (Math.abs(proteinRatio + carbRatio + fatRatio - 1) > 1e-6) {
-    throw new Error('Ratios must sum to 1');
-  }
-  const protein = (calories * proteinRatio) / 4;
-  const carbs = (calories * carbRatio) / 4;
-  const fats = (calories * fatRatio) / 9;
-  return { protein, carbs, fats };
-}
